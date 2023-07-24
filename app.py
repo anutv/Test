12ad11e395362ef878e1f8723cc1f1750a135727
Anut Verma <anutverma0716@gmail.com>
2023-07-25 01:31:39 +0530

    new stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import nltk
-import openai  # Import the OpenAI library
+import openai
 from flask import Flask, render_template, request
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
@@ -65,30 +65,30 @@ def get_openai_answer(user_question):
     return response.choices[0].text.strip()
 
 # Main function to analyze questions and answers
-def analyze_qna(file_path, user_question):
+def analyze_qna(file_path, user_question, memory):
     questions, answers = read_qna_file(file_path)
     question_similarities = []
 
     user_question_words = tokenize_text(user_question)
     processed_user_question = ' '.join(preprocess_text(user_question_words))
 
-    # Calculate cosine similarity for each question
-    for question in questions:
-        question_words = tokenize_text(question)
-        processed_question = ' '.join(preprocess_text(question_words))
-        similarity = calculate_similarity(processed_user_question, processed_question)
-        question_similarities.append(similarity[0][0])
+    # Check if the user's question is similar to any previous question in memory
+    for i, (mem_question, mem_answer) in enumerate(memory):
+        similarity = calculate_similarity(processed_user_question, mem_question)
+        question_similarities.append((i, similarity[0][0]))
 
-    max_similarity = max(question_similarities)
+    max_similarity = max(question_similarities, key=lambda x: x[1])
 
-    if max_similarity >= 0.75:
-        # If the highest similarity is above 0.75, get the most relevant answer
-        most_relevant_index = question_similarities.index(max_similarity)
-        most_relevant_answer = answers[most_relevant_index]
-        return most_relevant_answer
+    if max_similarity[1] >= 0.75:
+        # If the highest similarity is above 0.75, retrieve the previous response from memory
+        prev_response = memory[max_similarity[0]][1]
+        return prev_response
     else:
         # Otherwise, use OpenAI API to get the response
-        return get_openai_answer(user_question)
+        new_response = get_openai_answer(user_question)
+        # Store the user's question and the new response in memory
+        memory.append((processed_user_question, new_response))
+        return new_response
 
 @app.route('/')
 def index():
@@ -98,8 +98,19 @@ def index():
 def ask_question():
     user_question = request.form['question']
     file_path = "data.txt"  # Update with the path to your Q&A text file
-    answer = analyze_qna(file_path, user_question)
+
+    # Initialize or retrieve the memory list from session
+    if 'memory' not in session:
+        session['memory'] = []
+    memory = session['memory']
+
+    answer = analyze_qna(file_path, user_question, memory)
+
+    # Store the updated memory back to session
+    session['memory'] = memory
+
     return render_template('index.html', question=user_question, answer=answer)
 
 if __name__ == "__main__":
+    app.secret_key = 'your_secret_key'  # Replace with your own secret key
     app.run(debug=True)
